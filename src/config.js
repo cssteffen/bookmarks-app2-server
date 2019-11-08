fd5d70317c9851cc09aa53e2b237b92fabdb1665
Cara Steffen <cara@treadstonedesign.com>
2019-11-08 13:00:31 -0700

    deploying to heroku

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_KEY: process.env.API_KEY || "default-api",
-  DB_URL:
-    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bookmarks"
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/bookmarks"
 };
